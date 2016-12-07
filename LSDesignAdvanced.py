762a40e368cf1b60757f0f8a43ec6f2f568cc0aa
fourier-being <being.fourier@gmail.com>
2016-12-07 22:23:37 +0530

    Add Pass2 Stop2 and some other adjustments

--- LSDesignAdvanced.py
+++ LSDesignAdvanced.py
@@ -3,8 +3,10 @@ import LSFIR
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
 
-Fpass = 11.0     # MHz Passband end frequency
-Fstop = 15.0     #MHz Stopband start frequency
+Fpass2 = 11.0     # MHz Passband end frequency
+Fstop2 = 15.0     #MHz Stopband start frequency
+Fstop1 = 5.0
+Fpass1 = 4.0
 Fsamp = 50.0     # MHz Sampling Frequency
 Weight = 100    # Weight of stop band error
 Taps = 71      # FIR Filter Taps
@@ -14,12 +16,12 @@ Fnotch2 = 60
 h_global = np.zeros(Taps)
 design = 0
 
-fig, myFilter = plt.subplots(figsize=(20, 10))
-plt.subplots_adjust(left=0.25,bottom=0.25)
+fig, myFilter = plt.subplots(figsize=(18, 9))
+plt.subplots_adjust(left=0.25,bottom=0.3)
 myFilter.set_xlabel('Freq(MHz)')
 myFilter.set_ylabel('Magnitude(dB)')
  
-h = LSFIR.lpfls(Taps,2*np.pi*(Fpass/Fsamp),2*np.pi*(Fstop/Fsamp),Weight)
+h = LSFIR.lpfls(Taps,2*np.pi*(Fpass2/Fsamp),2*np.pi*(Fstop2/Fsamp),Weight)
 h_quant = np.round(h)
 h_global = h_quant
 H = np.fft.fft(h,1024)
@@ -34,18 +36,22 @@ plt.axis([0, 50.0, -50, 100])
 l2, = plt.plot(freq,Mag_quant, lw=2, color='red')
 
 axcolor = 'lightgoldenrodyellow'
-axTaps = plt.axes([0.6, 0.125, 0.25, 0.02], axisbg=axcolor)
-axFsamp = plt.axes([0.6, 0.1, 0.25,0.02], axisbg=axcolor)
-axFpass = plt.axes([0.6, 0.075, 0.25, 0.02],axisbg=axcolor)
-axFstop = plt.axes([0.6, 0.05,0.25, 0.02],axisbg=axcolor)
-axW = plt.axes([0.1, 0.125, 0.25, 0.02],axisbg=axcolor)   
-axFnotch1 = plt.axes([0.1, 0.1, 0.25, 0.02],axisbg=axcolor)  
-axFnotch2 = plt.axes([0.1, 0.075, 0.25, 0.02],axisbg=axcolor)
+axTaps = plt.axes([0.6, 0.175, 0.25, 0.02], axisbg=axcolor)
+axFsamp = plt.axes([0.6, 0.150, 0.25,0.02], axisbg=axcolor)
+axFpass2 = plt.axes([0.6, 0.125, 0.25, 0.02],axisbg=axcolor)
+axFstop2 = plt.axes([0.6, 0.100,0.25, 0.02],axisbg=axcolor)
+axFstop1 = plt.axes([0.6, 0.075,0.25, 0.02],axisbg=axcolor)
+axFpass1 = plt.axes([0.6, 0.050,0.25, 0.02],axisbg=axcolor)
+axW = plt.axes([0.1, 0.175, 0.25, 0.02],axisbg=axcolor)   
+axFnotch1 = plt.axes([0.1, 0.150, 0.25, 0.02],axisbg=axcolor)  
+axFnotch2 = plt.axes([0.1, 0.125, 0.25, 0.02],axisbg=axcolor)
 
 sTaps = Slider(axTaps,'Taps',15,200,valinit=Taps)
 sFsamp = Slider(axFsamp,'Samp Freq',1.0,100,valinit=Fsamp)
-sFpass = Slider(axFpass,'Passband',1.0,40.0,valinit=Fpass)
-sFstop = Slider(axFstop,'Stopband',1.0,45.0,valinit=Fstop)
+sFpass1 = Slider(axFpass1,'PassFreq2',1.0,40.0,valinit=Fpass1)
+sFstop1 = Slider(axFstop1,'StopFreq2',1.0,45.0,valinit=Fstop1)
+sFstop2 = Slider(axFstop2, 'StopFreq1',1.0,45.0,valinit=Fstop2)
+sFpass2 = Slider(axFpass2,'PassFreq1',1.0,45.0,valinit=Fpass2)
 sW = Slider(axW,'Weight',1.0,1000,valinit=Weight)
 sFnotch1 = Slider(axFnotch1,'Notch 1',0.0,60,valinit=Fnotch1)
 sFnotch2 = Slider(axFnotch2,'Notch 2',0.0,60,valinit=Fnotch2)
@@ -56,17 +62,21 @@ def update(val):
     if(Taps%2 == 0) :
         Taps = Taps +1 
     Fsamp = sFsamp.val
-    Fpass = sFpass.val
-    Fstop = sFstop.val
+    Fpass1 = sFpass1.val
+    Fstop1 = sFstop1.val
+    Fstop2 = sFstop2.val
+    Fpass2 = sFpass2.val
     W = sW.val
     Fnotch1 = sFnotch1.val
     Fnotch2 = sFnotch2.val
     
-    wp = 2*np.pi*(Fpass/Fsamp)
-    ws = 2*np.pi*(Fstop/Fsamp)
+    wp = 2*np.pi*(Fpass2/Fsamp)
+    ws = 2*np.pi*(Fstop2/Fsamp)
+    wp2 = 2*np.pi*(Fpass1/Fsamp)
+    ws2 = 2*np.pi*(Fstop1/Fsamp)
     wn1 = 2*np.pi*(Fnotch1/Fsamp)
     wn2 = 2*np.pi*(Fnotch2/Fsamp)
-    #h = np.zeros(0,Taps)
+
     if(design == 0):
         if((Fnotch1 <= Fsamp/2) and (Fnotch2 <= Fsamp/2)) :
             h = LSFIR.lpfls2notch(Taps,wp,ws,wn1,wn2,W)
@@ -77,9 +87,9 @@ def update(val):
         else:
             h = LSFIR.lpfls(Taps,wp,ws,W)
     elif(design == 1):
-        h = LSFIR.bpfls(Taps, 0.04, 0.07, wp, ws, W)
+        h = LSFIR.bpfls(Taps,ws2,wp2,wp,ws,W)
     elif(design == 2):
-        h = LSFIR.hpfls(Taps, ws, wp, W)
+        h = LSFIR.hpfls(Taps, ws2, wp2, W)
     h_quant = np.round(h)
     global h_global
     h_global = h_quant
@@ -98,8 +108,10 @@ def update(val):
     fig.canvas.draw_idle()
 sTaps.on_changed(update)
 sFsamp.on_changed(update)
-sFpass.on_changed(update)
-sFstop.on_changed(update)
+sFpass1.on_changed(update)
+sFstop1.on_changed(update)
+sFpass2.on_changed(update)
+sFstop2.on_changed(update)
 sW.on_changed(update)
 sFnotch1.on_changed(update)
 sFnotch2.on_changed(update)
@@ -110,8 +122,10 @@ button1 = Button(resetax, 'Reset', color=axcolor, hovercolor='green')
 def reset(event):
     sTaps.reset()
     sFsamp.reset()
-    sFpass.reset()
-    sFstop.reset()
+    sFpass1.reset()
+    sFstop1.reset()
+    sFstop2.reset()
+    sFpass2.reset()
     sW.reset()
     sFnotch1.reset()
     sFnotch2.reset()
